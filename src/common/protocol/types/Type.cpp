08e06d6f6f89d380ae7dd1d3ad95fe3a20c0f873
smartdu <smartdu@qq.com>
2019-05-13 16:01:20 +0800

    memory leak

--- src/common/protocol/types/Type.cpp
+++ src/common/protocol/types/Type.cpp
@@ -45,49 +45,49 @@ std::list<Type*> Type::values()
 DocumentedType* Type::BOOLEAN()
 {
 	static DocumentedType *once = new DocumentType_BOOLEAN;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::INT8()
 {
 	static DocumentedType *once = new DocumentedType_INT8;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::INT16()
 {
 	static DocumentedType *once = new DocumentedType_INT16;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::INT32()
 {
 	static DocumentedType *once = new DocumentedType_INT32;
-	return once;
+	return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::INT64()
 {
 	static DocumentedType *once = new DocumentedType_INT64;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::NULLABLE_BYTES()
 {
 	static DocumentedType *once = new DocumentedType_NULLABLE_BYTES;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::STRING()
 {
 	static DocumentedType *once = new DocumentedType_STRING;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 DocumentedType* Type::NULLABLE_STRING()
 {
 	static DocumentedType *once = new DocumentedType_NULLABLE_STRING;
-	return once;
+    return (DocumentedType*)once->clone();
 }
 
 std::string DocumentedType::toString()
