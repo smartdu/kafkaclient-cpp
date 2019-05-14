8449dae967ee9f3f5b89bf4a81470e0ebb90b42a
smartdu <smartdu@qq.com>
2019-05-13 21:44:08 -0400

    debug使用asan运行，release使用valgrind运行

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ PWD=`cd $(dirname $0); pwd`
 KPWD=$PWD
 
 //rm -rf .libs .build
-mkdir -p .libs .build
-cd $PWD/.build
+mkdir -p .libs .build/Debug .build/Release
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DD_SUFFIX=_d -DPLATFORM=CentOS-x64 -DCMAKE_INSTALL_PREFIX=$KPWD/.libs/ $KPWD 
+cd $KPWD/.build/Debug
+cmake -DCMAKE_BUILD_TYPE=Debug -DD_SUFFIX=_d -DPLATFORM=CentOS-x64 -DCMAKE_INSTALL_PREFIX=$KPWD/.libs/ $KPWD
+make && make install
+
+cd $KPWD/.build/Release
+cmake -DCMAKE_BUILD_TYPE=Release -DD_SUFFIX= -DPLATFORM=CentOS-x64 -DCMAKE_INSTALL_PREFIX=$KPWD/.libs/ $KPWD
 make && make install
