7d284f3a7c9d1f644e61028c091f5513f611ec82
smartdu <smartdu@qq.com>
2019-05-13 21:53:56 -0400

    debug使用asan运行，release使用valgrind运行

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ KPWD=$PWD
 
 #Debug
 cd $KPWD/.libs/bin
-LD_LIBRARY_PATH=$KPWD/.libs/lib $KPWD/.libs/bin/test
+LD_LIBRARY_PATH=$KPWD/.libs/lib $KPWD/.libs/bin/test_d
 
 echo "--------------------------------很长的分割线--------------------------------"
 
 #Release
 cd $KPWD/.libs/bin
-LD_LIBRARY_PATH=$KPWD/.libs/lib valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all $KPWD/.libs/bin/test_d
+LD_LIBRARY_PATH=$KPWD/.libs/lib valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all $KPWD/.libs/bin/test
 
 
 
